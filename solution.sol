357a2c65648809257f840086cec105c650f8b4c2
Shiv Chauhan <shiv.chauhan1518@gmail.com>
2024-06-24 23:14:37 +0530

    Add solution.sol

---
+++ solution.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Noni is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("Noni", "NOI") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
+
+// 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+// 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+
